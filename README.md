246eed85ea9ede8387b7c35d0b36db9949237656
Abel Soares Siqueira <abel.s.siqueira@gmail.com>
2017-11-09 08:51:46 -0200

    First commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# JILL - Julia Installer 4 Linux - Light
+
+Simply install latest [Julia](https://julialang.org).
+
+On Linux, the best way to install Julia is to use the Generic Linux
+Binaries. And while **all Linux users** love manually downloading,
+unpacking, and linking their software, this script does it for you.
+
+Simply run
+
+    sh -c "$(curl -fsSL https://raw.githubusercontent.com/abelsiqueira/jill/master/jill.sh)"
+
+## LICENSE
+
+This script is licensed under the GNU GPLv3 (see
+[LICENSE.md](LICENSE.md)). This dosn't affect your Julia usage at all.
---
+++ jill.sh
@@ -0,0 +1,89 @@
+#! /bin/sh
+#
+# jill.sh
+# Copyright (C) 2017 Abel Soares Siqueira <abel.s.siqueira@gmail.com>
+#
+# Distributed under terms of the GPLv3 license.
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+JULIA_DOWNLOAD=${JULIA_DOWNLOAD:-"$HOME/packages/julias"}
+JULIA_INSTALL=${JULIA_INSTALL:-"/usr/local/bin"}
+
+function header() {
+  echo "Jill - Julia Installer 4 Linux - Light"
+  echo "Copyright (C) 2017 Abel Soares Siqueira <abel.s.siqueira@gmail.com>"
+  echo "Distributed under terms of the GPLv3 license."
+}
+
+function badfolder() {
+  echo "The folder '$JULIA_INSTALL' is not on your PATH, you can"
+  echo "- 1) Add it to your path; or"
+  echo "- 2) Run 'JULIA_INSTALL=otherfolder ./jif.sh'"
+}
+
+function hi() {
+  header
+  if [[ ! ":$PATH:" == *":$JULIA_INSTALL:"* ]]; then
+    badfolder
+    [[ "$0" == "$BASH_SOURCE" ]] && exit 1 || return 1
+  fi
+  echo "This script will:"
+  echo ""
+  # TODO: Expand to install older Julia?
+  echo "  - Download latest stable Julia"
+  echo "  - Create a link for julia"
+  echo "  - Create a link for julia-VER"
+  echo ""
+  echo "Download folder: $JULIA_DOWNLOAD"
+  echo "Link folder: $JULIA_INSTALL"
+  echo ""
+  if [ ! -d $JULIA_DOWNLOAD ]; then
+    echo "Download folder will be created if required"
+  fi
+  if [ ! -w $JULIA_INSTALL ]; then
+    echo "You'll be asked for your sudo password to install on $JULIA_INSTALL"
+  fi
+}
+
+function confirm() {
+  read -p "Do you accept these terms? (Y/N) " -n 1 -r
+  echo
+  if [[ ! $REPLY =~ ^[Yy] ]]; then
+    echo "Aborted"
+    [[ "$0" == "$BASH_SOURCE" ]] && exit 1 || return 1
+  fi
+}
+
+function download_and_install() {
+  mkdir -p $JULIA_DOWNLOAD
+  cd $JULIA_DOWNLOAD
+  wget https://julialang.org/downloads/ -O page.html
+  url=$(grep "https.*linux/x64.*gz" page.html -m 1 -o)
+  [[ $url =~ julia-(.*)-linux ]] && version=${BASH_REMATCH[1]}
+  major=${version:0:3}
+  wget -c $url -O julia-$version.tar.gz
+  mkdir -p julia-$version
+  tar zxf julia-$version.tar.gz -C julia-$version --strip-components 1
+  sudo rm -f $JULIA_INSTALL/julia{,-$major,-$version}
+  julia=$PWD/julia-$version/bin/julia
+  sudo ln -s $julia $JULIA_INSTALL/julia
+  sudo ln -s $julia $JULIA_INSTALL/julia-$major
+  sudo ln -s $julia $JULIA_INSTALL/julia-$version
+}
+
+# --------------------------------------------------------
+
+hi
+confirm
+download_and_install
