a0a43511405edd8d6f9976a681afcfd9054c19fe
Jose Storopoli <43353831+storopoli@users.noreply.github.com>
2021-12-30 14:14:43 +0000

    Update ci-test.sh

--- ci-test.sh
+++ ci-test.sh
@@ -31,7 +31,7 @@ bash jill.sh -y
 
 msg "Installing Julia LTS version"
 bash jill.sh -y --lts
-[[ $(julia-1.6.4 -v) == "julia version 1.6.4" ]]
+[[ $(julia-1.6.5 -v) == "julia version 1.6.5" ]]
 
 julia -e 'using Pkg; Pkg.update(); Pkg.add("Example")'
 
