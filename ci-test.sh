69dea448117f6b4bb9b161259a7b92ac0e0061e6
Abel Soares Siqueira <abel.s.siqueira@gmail.com>
2022-08-21 23:07:04 +0200

    Keep permissions and ownership when upgrading

--- ci-test.sh
+++ ci-test.sh
@@ -10,7 +10,7 @@ msg() {
 }
 
 msg "Installing latest Julia"
-echo "yy" | bash jill.sh
+echo "yy" | $SUDO bash jill.sh
 
 msg "Testing julia is installed"
 julia -v
@@ -25,27 +25,28 @@ msg "Testing julia-x-y is installed"
 julia-"$major" -v
 
 msg "Installing specific Julia version"
-echo "yy" | bash jill.sh --version "$VERSION"
+echo "yy" | $SUDO bash jill.sh --version "$VERSION"
 julia-"$VERSION" -v
 
 msg "Testing if the version is correct"
 [[ $(julia-"$VERSION" -v) == "julia version $VERSION" ]]
 
 msg "Installing latest Julia without interactive prompt"
-bash jill.sh -y
+$SUDO bash jill.sh -y
 
 msg "Installing Julia LTS version"
-bash jill.sh -y --lts
+$SUDO bash jill.sh -y --lts
 [[ $(julia-1.6.7 -v) == "julia version 1.6.7" ]]
 
 julia -e 'using Pkg; Pkg.update(); Pkg.add("Example")'
 
 msg "Upgrading to the latest Julia"
-bash jill.sh -y -u 1.6.4
+$SUDO bash jill.sh -y -u 1.6
 julia -e 'using Pkg; Pkg.status()' | grep Example
+julia -e 'using Pkg; Pkg.update()' # Issue 73
 
 msg "Installing release candidate version"
-bash jill.sh --rc -y
+$SUDO bash jill.sh --rc -y
 RC=$(julia -v | cut -d' ' -f3)
 
 msg "Testing if the version is correct"
