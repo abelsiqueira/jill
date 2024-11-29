619199293aedfde4d2179ec03b645b085e7118e4
abelsiqueira <1068752+abelsiqueira@users.noreply.github.com>
2024-11-29 01:11:02 +0000

    :robot: Auto-update Julia versions

--- jill.sh
+++ jill.sh
@@ -16,7 +16,7 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-JULIA_LTS=1.10.6
+JULIA_LTS=1.10.7
 JULIA_LATEST=1.11
 
 function usage() {
